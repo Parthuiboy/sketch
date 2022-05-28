4d7ab8ba6bda89f59a39c7c125570f1cc4634bda
Parthuiboy <106093774+Parthuiboy@users.noreply.github.com>
2022-05-28 08:47:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+function preload(){
+    classifier = ml5.imageClassifier("DoodleNet");
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+    background("white");
+}
+
+function draw(){
+    strokeWeight(13);
+    stroke(0);
+    if(mouseIsPressed){
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+}
+
+function clearCanvas(){
+    background("white");
+}
+
+function classifyCanvas(){
+    classifier.classify(canvas, gotResults);
+}
+
+function gotResults(error, results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById("label").innerHTML = 'Label: ' + results[0].label;
+    document.getElementById("confidence").innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
+    utterthis = new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterthis);
+}
